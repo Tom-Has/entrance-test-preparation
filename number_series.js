32b9ac175cd2c1c0531f356d2effe06bfb08da30
Tom-Has <75426771+Tom-Has@users.noreply.github.com>
2020-12-14 09:47:35 +0100

    Update number_series.js

--- number_series.js
+++ number_series.js
@@ -1,4 +1,4 @@
-//Objekt-Konstruktor für Umrechungsfaktoren in der Zahlenreihe
+//object constructor for a single number including an operator within the series
 function series() {
 	this.zahl = Math.ceil(Math.random()*10);
 	this.opdet = Math.round(Math.random()*3);
@@ -11,25 +11,33 @@ function series() {
 	else if(this.opdet == 3)
 		this.operator = '/';
 	this.put = function(x) {
-		if(this.operator == '/' && x%this.zahl != 0)	//ändere Division auf Addition, falls kein ganzzahliges Ergebnis
+		//change division to addition in case division does not yield a whole number
+		if(this.operator == '/' && x%this.zahl != 0)
 			this.operator = '+';
 		return x = eval(x+this.operator+this.zahl);
 	}
 }
 
-//Erstellen einer Zahlenreihe
+//creating an actual number series together with the according solution
 function task_series() {
-	var schritte = 5;	//Anzahl der duchgeführten Schritte wird willkürlich festgelegt
-	var start = Math.ceil(Math.random()*10);	//Startvariable
-	var opnum = Math.ceil(Math.random()*(schritte-1));	//zufällig erstelle Anzahl an Umrechungen
+	//random number of steps including he computation of the solution
+	var schritte = 5;
+	//randomly assigned starting number
+	var start = Math.ceil(Math.random()*10);
+	//variable number of possible operations, one less than numbers
+	var opnum = Math.ceil(Math.random()*(schritte-1));
+	//array for the objects as described above
 	var oparray = [];
 	var count = 0;
+	//starting variable for output string
 	var tz = ' &rarr; ';
 	var ausgabe = start;
-	for(var i = 0; i < opnum; ++i) 			//Array mit Umrechungsobjekten füllen
+	//fill the 
+	for(var i = 0; i < opnum; ++i)
 		oparray[i] = new series;
-	for(var j = 0; j < schritte; ++j) {		//durch Schrittanzahl durchiterieren
-		if(count >= opnum)					//zusätzliche Steuervariable, die dem Array angepasst ist
+	for(var j = 0; j < schritte; ++j) {
+		//one time correction because the count variable increases further than the opnum array size
+		if(count >= opnum)
 			count = 0;
 		start = oparray[count].put(start);
 		if(j != schritte-1)
@@ -44,7 +52,7 @@ function task_series() {
 	document.getElementById("question").innerHTML = ausgabe;
 }
 
-//Kontrolle der Eingabe und Anzeige der Lösung
+//controlling user input for correct solution and adapt output accordingly
 function solution_series() {
 	var vorschlag = document.getElementById("enter").value;
 	var loesung = document.getElementById("solution").value;
