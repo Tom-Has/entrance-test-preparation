7aff073840f2fc23d8f2f6e6fdf0c5bf4e680891
Tom-Has <75426771+Tom-Has@users.noreply.github.com>
2020-12-14 09:43:44 +0100

    Update estimate.js

--- estimate.js
+++ estimate.js
@@ -1,51 +1,64 @@
-//allgemeine Variable für die Anzahl unterschiedlicher Aufgabentypen
+//general variable for the number of different task types
 var typenum = 6;
 
-//Intervall für Menge an Zahlen einer Formel, hier mit 2 und 5 willkürlich festgesetzt
+//interval for the amount of numbers within a formula, here set arbitrarily
 var min_amount = 2;
 var max_amount = 5;
 
-//Intervall für diverse Zahlenbereiche, sollen Potenzen von 10 sein, um Inkonsistenzen bei nachfolgenden Manipulationen zu vermeiden
+//interval for several number ranges to avoid inconsistencies, should be potences of 10 as recommended
 var min_value = 10;
 var max_value = 1000;
 
-//Objektkonstruktor unterteilt in unterschiedliche Aufgabentypen
+//constructor with divisions for different types of tasks
 function estimate() {
 	this.define = Math.floor(Math.random() * typenum)
 	this.result = '';
 	this.wrong = [];
-	if(this.define == 0) {									//Abschnitt für Dezimalzahl Rechnungen
-		this.amount = (Math.floor(Math.random() * (max_amount - min_amount))) + min_amount;	//Anzahl der Elemente in der Formel
+	//division decimal calculations
+	if(this.define == 0) {
+		//number of elements within the total formula 
+		this.amount = (Math.floor(Math.random() * (max_amount - min_amount))) + min_amount;
+		//random number of decimal places
 		this.prec = Math.ceil(Math.random() * 3);
-		this.op = '';													
-		this.inter = 0;													//Zwischenwert, beachte: wird ausserhalb Erstellung des Ausgabe-Strings eigentlich nicht gebraucht, muss keine Instanzvariable sein und müsste nur in put-Funktion vorkommen
+		//operator string
+		this.op = '';
+		//intermediate variable, not needed outside of the output string, need not be an instance variable and is required only inside the put function
+		this.inter = 0;
 		this.first = true;
-		this.put = function() {											//Ausgabefunktion, die außerdem Ergebnis manipuliert und falsche Ergebnisse generiert
-			var output = '';											// return Variable
+		//output function for html string including generation of incorrect answers
+		this.put = function() {
+			//starting string
+			var output = '';
 			for(var i = 0; i < this.amount; ++i) {
-				if(!this.first) {										//Unterscheidung übrige Ausgabelemente vom ersten
-					if((Math.ceil(Math.random() * 10)) % 2 == 0)		//zufällige Festlegung ob Addition oder Subtraktion der folgenden Formelelemente
+				//checking whether and element is the first in the formula, therefore no operator should be output
+				if(!this.first) {
+					//random assignment for addition or subtraction of the following formula elements
+					if((Math.ceil(Math.random() * 10)) % 2 == 0)
 						this.op = '+';
 					else
 						this.op = '-';
-					this.inter = (((Math.random() * (max_value - min_value))) + min_value).toFixed(this.prec);	//zufällige Festlegung eines Formelelements
-					this.result = eval(this.result+this.op+this.inter).toFixed(this.prec);	//forlaufendes Mitrechnen des echten Ergebnisses
-					output += ' '+this.op+' '+this.inter;				//Ausgabe-String erweitern
+					this.inter = (((Math.random() * (max_value - min_value))) + min_value).toFixed(this.prec);
+					//eval calculates the result from the intermediate result and the radnom new value with the operator in between
+					this.result = eval(this.result+this.op+this.inter).toFixed(this.prec);
+					//the according string is composed from the same values as above
+					output += ' '+this.op+' '+this.inter;
 				}
-				else {													//hier wird der erste Fall behandelt, Schritte sinngemäß wie oben beschrieben
+				else {
 					this.first = false;
 					this.inter = (((Math.random() * (max_value - min_value))) + min_value).toFixed(this.prec);	
 					this.result = this.inter;
 					output += this.inter;
 				}
 			}
-			this.wrong[0] = eval(this.result + '+' + '0.1');			//Generierung falscher Ergebnisse, in allen folgenden Fällen total willkürlich
+			//wrong results are generated with arbitrary and not particularly creative parameters
+			this.wrong[0] = eval(this.result + '+' + '0.1');			
 			this.wrong[1] = eval(this.result + '+' + '0.01');
 			this.wrong[2] = eval(this.result + '*' + '1.1').toFixed(this.prec);
-			return output + ' =';										//Ausgabe-String als Funktionsresultat
+			return output + ' =';
 		}
 	}
-	else if(this.define == 1) {								//Abschnitt für Quadrierung
+	//division squares
+	else if(this.define == 1) {								
 		this.elem = Math.ceil((Math.random() * ((max_value / 10) - min_value)) + min_value);
 		this.result = Math.pow(this.elem, 2);
 		this.wrong[0] = (this.result * 3.3).toFixed(); 
@@ -55,7 +68,8 @@ function estimate() {
 			return this.elem + '<sup>2</sup> =';
 		}
 	}
-	else if(this.define == 2) {								//Abschnitt für Wurzeln
+	//division square roots
+	else if(this.define == 2) {
 		this.result = Math.ceil((Math.random() * ((max_value / 10) - min_value)) + min_value);
 		this.wrong[0] = (this.result * 1.5).toFixed(); 
 		this.wrong[1] = (this.result * 1.1).toFixed();
@@ -64,7 +78,8 @@ function estimate() {
 			return 'Quadratwurzel von ' + Math.pow(this.result, 2)+ ' =';
 		}
 	}
-	else if(this.define == 3) {								//Abschnitt für Prozent-Rechnung
+	//division procent calculations
+	else if(this.define == 3) {								
 		this.relative = Math.random();
 		this.elem = Math.ceil((Math.random() * (max_value - min_value)) + min_value);
 		this.result = (this.relative * this.elem);
@@ -76,7 +91,8 @@ function estimate() {
 			return (this.relative*100).toFixed(2) + '% von ' + this.elem + ' =';
 		}
 	}
-	else if(this.define == 4) {								//Abschnitt für Multiplikation
+	//division multiplication
+	else if(this.define == 4) {								
 		this.amount = (Math.floor(Math.random() * (max_amount - min_amount))) + min_amount;
 		this.first = true;
 		this.inter = 0;
@@ -101,27 +117,35 @@ function estimate() {
 			return output + ' =';
 		}	
 	}
-	else if(this.define == 5) {								//Abschnitt Bruchrechnen
-		this.amount = (Math.floor(Math.random() * (max_amount - min_amount))) + min_amount;	//wie im ersten Abschnitt, zufällige Anzahl der Formelelemente
-		this.fraction = function() {									//Erstellung Subkonstruktor für Brüche
-			this.denom = Math.floor(Math.random() * (20 - 2)) + 2;		//Nenner, zufällig zwischen 2 und 20
-			this.num = Math.floor(Math.random() * (20 - 2)) + 2;		//Zähler, zufällig zwischen 2 und 20	
-			while(this.num >= this.denom) 								//Reduktion des Zählers um die Hälfte (willkürlich festgesetzt), bis er kleiner als der Nenner ist
+	//division rational numbers
+	else if(this.define == 5) {
+		//as in the first division
+		this.amount = (Math.floor(Math.random() * (max_amount - min_amount))) + min_amount;
+		//subconstructor for rational numbers
+		this.fraction = function() {									
+			this.denom = Math.floor(Math.random() * (20 - 2)) + 2;
+			this.num = Math.floor(Math.random() * (20 - 2)) + 2;
+			//arbirary reduction by 2 until numerator is less than denominator
+			while(this.num >= this.denom)
 				this.num = Math.round(this.num /= 2);
 			this.showfract = function() {
 				return this.num + '/' + this.denom;
 			}
 		}
-		this.collection = [];											//Anlegen eines Arrays, der nachfolgenden mit Bruch-Objekten befüllt wird
+		//array for rational number objects
+		this.collection = [];
 		for(var i = 0; i < this.amount; ++i)
 			this.collection[i] = new this.fraction();
-		this.put = function() {											//sinngemäße Funktion wie im ersten Abschnitt, inkl. Unterscheidung erstes Element von allen folgenden
+		//output function is principally the same as in the first division with adaptations to rational numbers
+		this.put = function() {
 			var output = '';
-			this.first = true;											//alternative Verwendung der boole'schen flag, zum testen ob das funktioniert
+			//this instance variable is for testing purposes
+			this.first = true;
 			var fract;
 			for(var i = 0; i < this.amount; ++i) {
 				if(!this.first) {
-					if((Math.ceil(Math.random() * 10)) % 2 == 0)		//wie Abschnitt 1, zufällige Festlegung ob Addition oder Subtraktion der folgenden Formelelemente
+					//random assignment of addition or subtraction as in the first division
+					if((Math.ceil(Math.random() * 10)) % 2 == 0)
 						this.op = '+';
 					else
 						this.op = '-';
@@ -135,11 +159,14 @@ function estimate() {
 					output += this.collection[i].showfract();
 				}
 			}
-			var kurz = Math.abs(ggt(fract.denom, fract.num)); 			//Absolutbetrag des größten gemeinsamen Teilers, gewöhnliche Funktion s. untem im Skript
-			fract.num /= kurz;											//Kürzen der beiden Anteile durch den ggT
+			//use of Math.abs() should not be necessary here, but has no impact on output or result
+			var kurz = Math.abs(ggt(fract.denom, fract.num));
+			//applying the gcd function to make the fraction approriately simple
+			fract.num /= kurz;
 			fract.denom /= kurz;
 			this.result = fract.showfract();
-			++fract.num; ++fract.denom;									//in Folge willkürliches Ändern des echten Ergebnisses
+			++fract.num; ++fract.denom;
+			//again rather uncreative variations of the correct result
 			this.wrong[0] = fract.showfract();
 			fract.num *= 2; fract.denom *= 3;
 			this.wrong[1] = fract.showfract();
@@ -150,23 +177,27 @@ function estimate() {
 	}
 }
 
-//Funktion zur Erstellung eines Schätzobjekts und Erstellen der Frageoptionen
+//function for creating the estimation task and transporting it into the according html elements
 function task_estimate() {
 	document.getElementById('solution').innerHTML = '';
-	x = new estimate(); 								//globale Variable, da in der Lösungsfunktion auf das Ergebnis des Objekts zugegriffen wird
+	//global variable for allowing access to the object from the solution function below
+	x = new estimate();
 	document.getElementById('question').innerHTML = x.put()+'<br><br>';
-	var antwort = [];									//Array mit den Antwortmöglichkeiten, in der Folge werden richtige und falsche Antworten zugewiesen
+	//array for possible answers, ids for wright and wrong answer(s) are assigned accordingly
+	var antwort = [];
 	antwort[0] = '<input type="radio" name="estim" id="right"> '+x.result;
 	antwort[1] = '<input type="radio" name="estim" id="wrong"> '+x.wrong[0];
 	antwort[2] = '<input type="radio" name="estim" id="wrong"> '+x.wrong[1];
 	antwort[3] = '<input type="radio" name="estim" id="wrong"> '+x.wrong[2];
-	antwort.sort(function(a, b){return 0.5 - Math.random()});	//zufällige Sortierung der Antwortmöglichkeiten
+	//random sorting of possible answers
+	antwort.sort(function(a, b){return 0.5 - Math.random()});	
 	for(var i = 0; i < 4; ++i)
 		document.getElementById('question').innerHTML += antwort[i]+'<br>';
-	//console.log(x.result);								//ein bisschen schummeln darf man ja auch ;)
+	//cheating is allowed for testing purposes
+	//console.log(x.result);								
 }
 
-//Funktion zum Abrufen der Lösung
+//checking solution and outputting accordingly
 function solution_estimate() {
 	if(document.getElementById('right').checked)
 		document.getElementById('solution').innerHTML = 'Korrekt!';
@@ -174,7 +205,7 @@ function solution_estimate() {
 		document.getElementById('solution').innerHTML = 'Falsch. Die korrekte Antwort lautet '+x.result;
 }
 
-//größter gemeinsamer Teiler selbst implementiert, für Abschnitt Bruchrechnen im estimate-Konstruktor
+//recursive function for the greatest common denominator, useful for rational number computations in the estimate-constructor
 function ggt(a, b) {
 	if(a % b == 0)
 		return b;
